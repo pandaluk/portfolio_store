3f04fba39a32ba73ebcb9d2359f9b17f29271f7b
Arthur <arthur.silva1@hotmail.com.br>
2020-04-14 22:23:18 -0300

    Init back-end

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,69 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = { 
+    id: uuid(), 
+    title, 
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return res.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  const repository = repositories.find(repository => repository.id == id);
+
+  if(!repository){
+    return res.status(400).json({ error: 'Repository not found.' })
+  }
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return res.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if(repositoryIndex < 0){
+    return res.status(400).json({ error: 'Repository not found.' })
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find(repository => repository.id == id);
+
+  if(!repository){
+    return res.status(400).json({ error: 'Repository not found.' })
+  }
+
+  repository.likes += 1;
+  
+  return res.json(repository);
 });
 
 module.exports = app;
